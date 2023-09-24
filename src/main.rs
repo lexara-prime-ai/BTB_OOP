5b3b6a554df8aade9d840b0c190e2e081caae2ec
projectfinalaudio <irfanghat@gmail.com>
2023-09-24 18:28:30 +0300

    Source files | Polymorphism in Rust

---
+++ src/main.rs
@@ -0,0 +1,57 @@
+/// Create Vehicle struct
+struct Vehicle;
+
+/// Implement the struct
+impl LandCapable for Vehicle {}
+
+impl WaterCapable for Vehicle {}
+
+/// Create and implement trait | LandCapable
+trait LandCapable {
+    /// The method will have a shared reference to itself
+    fn drive(&self) -> () {
+        println!("DEFAULT method implementation: Engine starting...");
+    }
+}
+
+/// Create and implement trait | WaterCapable
+trait WaterCapable {
+    fn float(&self) {
+        println!("DEFAULT method implementation: Pressurizing...");
+    }
+}
+
+// Create a trait for multi-terrain
+/// Force MultiTerrain trait to implement both WaterCapable & LandCapable -> referred to as super traits
+trait MultiTerrain: LandCapable + WaterCapable {}
+
+struct HoverCraft;
+
+impl MultiTerrain for HoverCraft {}
+
+impl LandCapable for HoverCraft {}
+
+impl WaterCapable for HoverCraft {}
+
+/// Create ignition method with reference to Vehicle
+#[allow(dead_code)]
+fn ignition(vehicle: &dyn LandCapable) {
+    vehicle.drive();
+}
+
+#[allow(dead_code)]
+fn pressurize(vehicle: &dyn WaterCapable) {
+    vehicle.float();
+}
+
+/// This method combines both methods that are implemented by the traits
+fn ignition_pressurize(vehicle: &dyn MultiTerrain) {
+    // Implement both methods
+    vehicle.drive();
+    vehicle.float();
+}
+
+fn main() {
+    let hover_craft: HoverCraft = HoverCraft;
+    ignition_pressurize(&hover_craft);
+}
